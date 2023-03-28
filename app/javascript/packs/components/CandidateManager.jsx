0a2e187c6d95398bb97634daa8b42c7cd8cdbd88
Giovanni Guadagnucci <giovanniguadag@gmail.com>
2023-03-28 18:50:36 -0300

    feat(issue-5):(adding error handling and loading states)

--- app/javascript/packs/components/CandidateManager.jsx
+++ app/javascript/packs/components/CandidateManager.jsx
@@ -4,23 +4,32 @@ import CandidateForm from './CandidateForm';
 import CandidateList from './CandidateList';
 import { toast } from 'react-hot-toast';
 
-
 const CandidateManager = () => {
     const [candidates, setCandidates] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchCandidates();
     }, []);
 
     const fetchCandidates = async () => {
-        const response = await axios.get('/api/v1/candidates');
-        setCandidates(response.data);
+        try {
+            setLoading(true);
+            const response = await axios.get('/api/v1/candidates');
+            setCandidates(response.data);
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+            toast.error('Failed to fetch candidates.');
+            setLoading(false);
+        }
     };
 
     const createCandidate = async (candidate) => {
         try {
             await axios.post('/api/v1/candidates', candidate);
             fetchCandidates();
+            toast.success('Candidate created successfully!');
         } catch (error) {
             console.error(error);
             toast.error('Failed to create candidate.');
@@ -28,13 +37,25 @@ const CandidateManager = () => {
     };
 
     const updateCandidate = async (id, candidate) => {
-        await axios.put(`/api/v1/candidates/${id}`, candidate);
-        fetchCandidates();
+        try {
+            await axios.put(`/api/v1/candidates/${id}`, candidate);
+            fetchCandidates();
+            toast.success('Candidate updated successfully!');
+        } catch (error) {
+            console.error(error);
+            toast.error('Failed to update candidate.');
+        }
     };
 
     const deleteCandidate = async (id) => {
-        await axios.delete(`/api/v1/candidates/${id}`);
-        fetchCandidates();
+        try {
+            await axios.delete(`/api/v1/candidates/${id}`);
+            fetchCandidates();
+            toast.success('Candidate deleted successfully!');
+        } catch (error) {
+            console.error(error);
+            toast.error('Failed to delete candidate.');
+        }
     };
 
     return (
@@ -44,11 +65,15 @@ const CandidateManager = () => {
                 createCandidate={createCandidate}
                 updateCandidate={updateCandidate}
             />
-            <CandidateList
-                candidates={candidates}
-                updateCandidate={updateCandidate}
-                deleteCandidate={deleteCandidate}
-            />
+            {loading ? (
+                <div className="text-center text-xl">Loading candidates...</div>
+            ) : (
+                <CandidateList
+                    candidates={candidates}
+                    updateCandidate={updateCandidate}
+                    deleteCandidate={deleteCandidate}
+                />
+            )}
         </div>
     );
 };
