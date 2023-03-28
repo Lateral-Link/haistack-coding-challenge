3d1541b01f66e25f3864f40a7c3c2253432229a8
Giovanni Guadagnucci <giovanniguadag@gmail.com>
2023-03-28 19:24:50 -0300

    feat(issue-5):(removing js errors)

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import CandidateManager from './components/CandidateManager'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import CandidateManager from './components/CandidateManager';
 import 'stylesheets/application.scss';
 import { Toaster } from 'react-hot-toast';
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
+  createRoot(document.getElementById('root')).render(
     <div className="container mx-auto px-4">
-      <Toaster position='bottom-center' />
+      <Toaster position="bottom-center" />
       <CandidateManager className="mt-4 sm:mt-8 md:mt-12 lg:mt-16 xl:mt-20" />
-    </div>,
-    document.getElementById('root')
+    </div>
   );
 });
