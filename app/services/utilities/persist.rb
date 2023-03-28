c420172a73ab719459a7912e46b4546367d0ca87
Giovanni Guadagnucci <giovanniguadag@gmail.com>
2023-03-28 06:56:03 -0300

    feat(issue-5):(persist database)

---
+++ app/services/utilities/persist.rb
@@ -0,0 +1,23 @@
+module Utilities
+  class Persist < BaseService
+    def initialize(repository)
+      @repository = repository
+    end
+
+    def call(args, action = nil, record = nil)
+      case action
+      when :create
+        record = @repository.create!(args)
+      when :update
+        record.assign_attributes(args)
+      when :delete
+        record.destroy
+      else
+        raise ArgumentError, "Invalid action: #{action.inspect}"
+      end
+      Success(record)
+    rescue StandardError
+      Failure(:internal_server_error)
+    end
+  end
+end
