5c33dc001e8bee2e57fae5f49f3192ffc9169434
Giovanni Guadagnucci <giovanniguadag@gmail.com>
2023-03-29 16:07:27 -0300

    feat(issue-5):(add empty list layout)

--- app/javascript/packs/molecules/CandidateList.jsx
+++ app/javascript/packs/molecules/CandidateList.jsx
@@ -39,80 +39,83 @@ const CandidateList = ({ candidates, updateCandidate, deleteCandidate }) => {
 
     return (
         <div className="flex items-center justify-center">
-            <table className="min-w-full text-center divide-y divide-gray-200">
-                <thead>
-                    <tr>
-                        <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                        <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                        <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Birth Date</th>
-                        <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white text-center divide-y divide-gray-200">
-                    {candidates.map((candidate) => (
-                        <tr key={candidate.id}>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                {editingCandidateId === candidate.id ? (
-                                    <Input
-                                        type="text"
-                                        value={editedCandidates[candidate.id]?.name || candidate.name}
-                                        onChange={(e) => handleInputChange(candidate.id, 'name', e.target.value)}
-                                        onKeyDown={(e) => handleKeyDown(e, candidate.id)}
-                                    />
-                                ) : (
-                                    candidate.name
-                                )}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                {editingCandidateId === candidate.id ? (
-                                    <Input
-                                        type="email"
-                                        value={editedCandidates[candidate.id]?.email || candidate.email}
-                                        onChange={(e) => handleInputChange(candidate.id, 'email', e.target.value)}
-                                        onKeyDown={(e) => handleKeyDown(e, candidate.id)}
-                                    />
-                                ) : (
-                                    candidate.email
-                                )}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                {editingCandidateId === candidate.id ? (
-                                    <Input
-                                        type="date"
-                                        value={editedCandidates[candidate.id]?.birth_date || candidate.birth_date}
-                                        onChange={(e) => handleDateInputChange(candidate.id, e.target.value)}
-                                        onKeyDown={(e) => handleKeyDown(e, candidate.id)}
-                                    />
-                                ) : (
-                                    candidate.birth_date
-                                )}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                {editingCandidateId === candidate.id ? (
-                                    <Button
-                                        onClick={() => handleSave(candidate.id)}
-                                        className="mr-2"
-                                    >
-                                        Save
-                                    </Button>
-                                ) : (
-                                    <Button
-                                        onClick={() => handleEdit(candidate.id)}
-                                        className="mr-2"
-                                    >
-                                        Edit
-                                    </Button>
-                                )}
-                                <Button
-                                    onClick={() => handleDelete(candidate.id)}
-                                >
-                                    Delete
-                                </Button>
-                            </td>
+            {candidates.length > 0 ? (
+                <table className="min-w-full text-center divide-y divide-gray-200">
+                    <thead>
+                        <tr>
+                            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Birth Date</th>
+                            <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="bg-white text-center divide-y divide-gray-200">
+                        {candidates.map((candidate) => (
+                            <tr key={candidate.id}>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    {editingCandidateId === candidate.id ? (
+                                        <Input
+                                            type="text"
+                                            value={editedCandidates[candidate.id]?.name || candidate.name}
+                                            onChange={(e) => handleInputChange(candidate.id, 'name', e.target.value)}
+                                            onKeyDown={(e) => handleKeyDown(e, candidate.id)}
+                                        />
+                                    ) : (
+                                        candidate.name
+                                    )}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    {editingCandidateId === candidate.id ? (
+                                        <Input
+                                            type="email"
+                                            value={editedCandidates[candidate.id]?.email || candidate.email}
+                                            onChange={(e) => handleInputChange(candidate.id, 'email', e.target.value)}
+                                            onKeyDown={(e) => handleKeyDown(e, candidate.id)}
+                                        />
+                                    ) : (
+                                        candidate.email
+                                    )}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    {editingCandidateId === candidate.id ? (
+                                        <Input
+                                            type="date"
+                                            value={editedCandidates[candidate.id]?.birth_date || candidate.birth_date}
+                                            onChange={(e) => handleDateInputChange(candidate.id, e.target.value)}
+                                            onKeyDown={(e) => handleKeyDown(e, candidate.id)}
+                                        />
+                                    ) : (
+                                        candidate.birth_date
+                                    )}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    {editingCandidateId === candidate.id ? (
+                                        <Button
+                                            onClick={() => handleSave(candidate.id)}
+                                            className="mr-2"
+                                        >
+                                            Save
+                                        </Button>
+                                    ) : (
+                                        <Button
+                                            onClick={() => handleEdit(candidate.id)}
+                                            className="mr-2"
+                                        >
+                                            Edit
+                                        </Button>
+                                    )}
+                                    <Button
+                                        onClick={() => handleDelete(candidate.id)}
+                                    >
+                                        Delete
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>) : (
+                <h2 className="text-2xl h-full text-gray-700">No Candidates registered</h2>
+            )}
         </div>
     );
 };
