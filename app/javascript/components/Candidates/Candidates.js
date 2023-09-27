8e7c0bbe841911c0dbcb0990521e7c9e93a5364e
Guilherme Monteiro <guilherme.monteiro@mobile2you.com.br>
2023-09-26 22:06:32 -0300

    refactor(React):(Listing candidates names)

--- app/javascript/components/Candidates/Candidates.js
+++ app/javascript/components/Candidates/Candidates.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState, useEffect, Fragment} from "react";
+import axios from "axios";
 
 const Candidates = () => {
-  return <div>This is the Candidates#index view for our app.</div>
+  const [candidates, setCandidates] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/v1/candidates.json')
+    .then(resp => {
+      setCandidates(resp.data)
+    })
+    .catch(resp => console.log(resp))
+
+  }, [candidates.length])
+
+  const list = candidates.map(item => {
+    return (<li key={item.name}>{item.name}</li>)
+  })
+
+  return (
+  <Fragment>
+  <div>This is the Candidates#index view for our app.</div>
+  <ul>{list}</ul>
+  </Fragment>
+  )
 }
 
 export default Candidates
