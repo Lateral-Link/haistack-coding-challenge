c14e5aeea9d5023c726f17c455ee01fb37ef7736
Andre Gomes <andre270492@gmail.com>
2024-02-08 13:31:47 -0300

    feat: add `@` alias for src folder

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+const path = require('path')
+
+// Add alias for @ symbol
+environment.config.set('resolve.alias', {
+  '@': path.resolve(__dirname, '..', '..', 'app/javascript/src')
+})
+
 module.exports = environment
