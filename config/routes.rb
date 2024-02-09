64b1f14fe6b3cb6a398b0c541e03c646f933c5a6
Andre Gomes <andre270492@gmail.com>
2024-02-09 13:13:19 -0300

    feat: add API controller and routes for creating candidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :candidates, only: %i[create]
+    end
+  end
 
   root 'home#index'
+  get '*path', to: 'home#index', via: :all
 end
