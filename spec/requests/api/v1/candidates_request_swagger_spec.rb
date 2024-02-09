1e653818773752ed4d3aa9377aaed0d60917aff3
Andre Gomes <andre270492@gmail.com>
2024-02-09 17:12:37 -0300

    chore: update candidates Swagger documentation

--- spec/requests/api/v1/candidates_request_swagger_spec.rb
+++ spec/requests/api/v1/candidates_request_swagger_spec.rb
@@ -4,17 +4,55 @@ require 'swagger_helper'
 
 describe 'Candidates API' do
   path '/api/v1/candidates' do
+    get 'Retrieves a list of candidates' do
+      tags 'Candidates'
+      produces 'application/json'
+      parameter name: :page, in: :query, type: :integer
+      parameter name: :per_page, in: :query, type: :integer
+
+      response '200', 'Candidates list' do
+        schema type:       :object,
+               properties: {
+                 candidates: {
+                   type:  :array,
+                   items: {
+                     type:       :object,
+                     properties: {
+                       id:        { type: :integer },
+                       name:      { type: :string },
+                       email:     { type: :string },
+                       birthdate: { type: :string, format: :date }
+                     }
+                   }
+                 },
+                 meta:       {
+                   type:       :object,
+                   properties: {
+                     page:        { type: :integer },
+                     per_page:    { type: :integer },
+                     total_pages: { type: :integer },
+                     total_count: { type: :integer }
+                   }
+                 }
+               }
+
+        let(:page) { 1 }
+        let(:per_page) { 20 }
+        run_test!
+      end
+    end
+
     post 'Creates a candidate' do
       tags 'Candidates'
       consumes 'application/json'
       parameter name: :candidate, in: :body, schema: {
-        type: :object,
+        type:       :object,
         properties: {
-          name: { type: :string, example: 'Andre' },
-          email: { type: :string, format: :email, example: 'andre@haistack.ai' },
+          name:      { type: :string, example: 'Andre' },
+          email:     { type: :string, format: :email, example: 'andre@haistack.ai' },
           birthdate: { type: :string, format: :date, example: '1992-04-27' }
         },
-        required: %w[name email birthdate]
+        required:   %w[name email birthdate]
       }
 
       response '201', 'Candidate created' do
