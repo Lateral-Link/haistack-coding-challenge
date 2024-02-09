690f2f9a1a14d7bd42099e4240501896d1a88b94
Andre Gomes <andre270492@gmail.com>
2024-02-09 11:36:13 -0300

    feat: add Candidates::CreateValidator

---
+++ app/validators/candidates/create_validator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Candidates
+  class CreateValidator < BaseValidator
+    validates :name, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :birthdate, presence: true
+    validate :past_birtdate
+
+    def past_birtdate
+      return if birthdate.blank?
+
+      errors.add(:birthdate, :future_date) if birthdate > Date.current
+    end
+  end
+end
