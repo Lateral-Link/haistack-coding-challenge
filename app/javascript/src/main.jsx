11544851fd03be1c6af77513af01519e1490ede9
Andre Gomes <andre270492@gmail.com>
2024-02-10 17:14:53 -0300

    i18n: add i18n configuration and en.json file

--- app/javascript/src/main.jsx
+++ app/javascript/src/main.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from '@/views/index.js'
+import i18n from '@/i18n/config.js'
+import { I18nextProvider } from 'react-i18next'
+
 import './main.css'
 
-const Main = (props) => (
-  <div className='main'>Hello {props.name}!</div>
-)
+const Main = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ])
 
-Main.defaultProps = {
-  name: 'World',
-}
-
-Main.propTypes = {
-  name: PropTypes.string,
+  return <I18nextProvider i18n={i18n}>
+    <RouterProvider router={router} />
+  </I18nextProvider>
 }
 
 export default Main
