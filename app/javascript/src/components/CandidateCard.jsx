a3b6bd3b99fffa25ebe287fe04a72c7e3439c2dc
Andre Gomes <andre270492@gmail.com>
2024-02-11 11:09:07 -0300

    feat: add CandidateCard component

--- app/javascript/src/components/CandidateCard.jsx
+++ app/javascript/src/components/CandidateCard.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { Card, Avatar, Flex, Typography } from 'antd'
+import { GiftOutlined, MailOutlined, UserOutlined } from '@ant-design/icons'
 
-const CandidateCard = ({ candidate }) => {
+const { Title } = Typography
+
+const CandidateCard = ({ candidate, loading }) => {
   return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{candidate.name}</h5>
-        <p className="card-text">{candidate.email}</p>
-      </div>
-    </div>
+    <Card loading={loading}>
+      <Flex gap={16}>
+        <Avatar size='large' icon={<UserOutlined />} />
+
+        <Flex gap={16} vertical>
+          <Title level={2}>{candidate.name}</Title>
+
+          <Flex gap={16}>
+            <MailOutlined style={{ fontSize: '20px' }} />
+            <a href={`mailto:${candidate.email}`} data-testid='email-link'>
+              {candidate.email}
+            </a>
+          </Flex>
+
+          <Flex gap={16}>
+            <GiftOutlined style={{ fontSize: '20px' }} />
+            <p>{candidate.birthdate?.format('YYYY/MM/DD')}</p>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Card>
   )
 }
 
