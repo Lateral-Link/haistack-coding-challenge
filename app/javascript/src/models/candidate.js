8fb88caddb0db1be8dfd36ce55f43cb00dab8b3b
Andre Gomes <andre270492@gmail.com>
2024-02-12 09:35:35 -0300

    chore: add candidateMeta function to candidate model

--- app/javascript/src/models/candidate.js
+++ app/javascript/src/models/candidate.js
@@ -6,3 +6,10 @@ export default (candidate) => ({
   email: candidate.email,
   birthdate: dayjs(candidate.birthdate),
 })
+
+export const candidateMeta = (meta) => ({
+  page: meta.page,
+  perPage: meta.per_page,
+  totalCount: meta.total_count,
+  totalPages: meta.total_pages,
+})
