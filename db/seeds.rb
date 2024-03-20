d5d8e9b4f77b1c2ef6a0d3fb4da8dc268c6f9ad3
Raquel Limâ <raquellimadev@gmail.com>
2024-03-19 22:12:50 -0300

    add candidates seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+
+puts 'Creating candidates...'
+
+created_count = 0
+10.times do
+  candidate = FactoryBot.create(:candidate)
+  if candidate.persisted?
+    puts "Candidate created: Name: #{candidate.name}, Email: #{candidate.email}, Date of Birth: #{candidate.date_of_birth}"
+    created_count += 1
+  end
+end
+
+puts "#{created_count} candidates created successfully." if created_count.positive?
